ed5d9de13dcda7189f1999203433dfb591b431b3
tina0035 <liuyan@weilaigongzuo.com>
2018-12-29 16:09:44 +0800

    fix: delete no use config

--- generators/app/templates/commitlint.config.js
+++ generators/app/templates/commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [2, 'always', [
-        "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
-      ]],
-      'type-case': [0,'always',['lowerCase']],
-      'type-empty': [0, 'never'],
-    }
+    extends: ['@commitlint/config-conventional']
   };
